b2372a190ce3075f4d5b8df3c21d66024c585f5b
Simone Guimaraes <simoneguimaraes11@gmail.com>
2021-10-19 18:23:13 -0300

    initial commit

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-// Seu código aqui!
-
 const products = [
   {
     id: 1,
@@ -23,4 +21,17 @@ const products = [
   },
 ];
 
-// Declare sua funçao aqui. Nao se esqueça de invocar a função para conseguir testar!
+
+function filterProducts(category) {
+ let tagApple = []
+  for(let i = 0; i < products.length; i++) {
+    for(let j = 0; j < products[i].tags.length; j++) {
+      if(products[i].tags[j] === category) {
+          tagApple.push(products[i]) 
+      }
+    }
+  }
+  console.log(tagApple)
+}
+
+filterProducts('apple')
